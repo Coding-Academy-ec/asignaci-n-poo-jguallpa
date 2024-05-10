db1ef04f923d93c94fd04895e4721349f4033e5f
Juan Guallpa <jguallpa@hotmail.com>
2024-05-09 21:58:16 -0500

    resolución ejercicio asignacion poo

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
+import math
+
 class Rectangulo:
     def __init__(self, base, altura):
         self.base = base
         self.altura = altura
 
     def area(self):
-        pass # implementación de la función con la forula de área de un rectángulo
+        areas= self.base * self.altura  # implementación de la función con la formula de área de un rectángulo
+        return areas
 
+rectangulo = Rectangulo(5, 7)
+print (f"Area del rectanguo e: {rectangulo.area()}")
 
 class Circulo:
     pi = 3.141592653589793
 
     def __init__(self, radio):
-        pass # inicialización de la variable radio
+        self.__radio= radio  # inicialización de la variable radio
 
     def area(self):
-        pass # implementación de la función con la forula de área de un círculo
+        areas = math.pi * self.__radio**2 # implementación de la función con la forula de área de un círculo
+        return areas
+    
+
+cir = Circulo(5)
+print(f"Area del circulo es: {cir.area()}")
